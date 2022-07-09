b7a9dd21f4dd1173a4d603d8f8fb280f4d837e5a
abdulazis09 <sapigoreng72@gmail.com>
2022-07-09 20:32:42 +0700

    Pengecekan Jika data tidak ditemukan

--- backend/controller/goalController.js
+++ backend/controller/goalController.js
@@ -11,6 +11,8 @@ module.exports = {
 //@route POST /api/goals
 //acces Private
 const setGoal = (req,res) => {
-    console.log(req.body)
+    if(!req.body.text){
+        res.status(400).json({message: 'Please add a text filed'})
+    }
 res.status(200).json({message: 'Get Goals'})
 }
