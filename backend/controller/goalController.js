66f24f6b9f745aeb6a79944a9f3bb937989f6f32
abdulazis09 <sapigoreng72@gmail.com>
2022-07-10 07:35:52 +0700

    All Express Async Handler

--- backend/controller/goalController.js
+++ backend/controller/goalController.js
@@ -12,11 +12,17 @@ module.exports = {
 //@desc Set goals 
 //@route POST /api/goals
 //acces Private
-const setGoal = (req,res) => {
+const setGoal = asyncHandler (async (req,res) => {
     if(!req.body.text){
         res.status(400)
         throw new Error ('Please add a text filed')  
     }
 
 res.status(200).json({message: 'Get Goals'})
-}
+})
+const upadateGoal = asyncHandler (async(req,res) => {
+    res.status(200).json({message: `Update Goal ${req.params.id}`})
+})
+const deletGoal = asyncHandler (async(req,res) => {
+    res.status(200).json({message: `Delet Goal ${req.params.id}`})
+})
