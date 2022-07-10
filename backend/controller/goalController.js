6c855ae59ec9bce647d30ebb6ff4eb031297cebd
abdulazis09 <sapigoreng72@gmail.com>
2022-07-10 07:02:44 +0700

    menambah bkok eror

--- backend/controller/goalController.js
+++ backend/controller/goalController.js
@@ -12,7 +12,9 @@ module.exports = {
 //acces Private
 const setGoal = (req,res) => {
     if(!req.body.text){
-        res.status(400).json({message: 'Please add a text filed'})
+        res.status(400)
+        throw new Error ('Please add a text filed')  
     }
+
 res.status(200).json({message: 'Get Goals'})
 }
