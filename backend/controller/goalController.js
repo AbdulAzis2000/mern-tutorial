5d101348f7c4a6f229af31bb5102d933ba24da38
abdulazis09 <sapigoreng72@gmail.com>
2022-07-11 19:25:02 +0700

    delete goal mongodb

--- backend/controller/goalController.js
+++ backend/controller/goalController.js
@@ -42,3 +42,23 @@ const updateGoal = asyncHandler (async(req,res) => {
 })
 // @desc    Delete goal
 // @route   DELETE /api/goals/:id
+// @route   DELETE /api/goals/:id
+// @access  Private 
+const deleteGoal = asyncHandler (async(req,res) => {
+    res.status(200).json({message: `Delete Goal ${req.params.id}`})
+    const goal = await Goal.findById(req.params.id)
+    if(!goal) {
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+
+    await goal.remove()
+
+    res.status(200).json({id: req.params.id})
+})
+module.exports = {
+    getGoals,
+    setGoal,
+    updateGoal,
+    deleteGoal
+}
